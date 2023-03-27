b802b5b313016c0ed05629340084382aef0142e2
Andy Knight <andym.knight@gmail.com>
2023-03-27 14:15:12 +0100

    styling

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,34 @@
+import type { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.firstName + "" + user.lastName,
+    image: user.profileImageUrl,
+  };
+};
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: { createdAt: "desc" },
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((p) => p.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((p) => ({
+      post: p,
+      author: users.find((u) => u.id === p.authorId),
+    }));
   }),
 });
