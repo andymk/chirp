acaf2f34b81b484d7fd9e48559987933c879d01a
Andy Knight <andym.knight@gmail.com>
2023-03-27 14:44:35 +0100

    dayjs added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,21 @@ import { api, RouterOutputs } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
   if (!user) return null;
-  console.log(user);
+
   return (
     <div className="flex w-full gap-x-3">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="User Profile Image"
         className="h-14 w-14 rounded-full"
+        width={54}
+        height={54}
       />
       <input
         type="text"
@@ -37,7 +40,13 @@ const PostView = (props: PostWithUser) => {
       key={post.id}
       className="flex items-center gap-4 border-b border-slate-400 p-4"
     >
-      <img src={author?.image} className="h-14 w-14 rounded-full" />
+      <Image
+        src={author?.image!}
+        className="h-14 w-14 rounded-full"
+        alt=""
+        width={54}
+        height={54}
+      />
       <div className="flex flex-col">
         <div className="flex text-slate-400">
           <span>{`@${author?.name}`}</span>
